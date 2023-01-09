3ac7d35f8fd07eaae347f4012bc2c2a192bfa5f5
OtisChaka <113614794+OtisChaka@users.noreply.github.com>
2023-01-09 13:29:02 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    This is a real thing
+    <div class="w-10 h-10 bg-red-600"></div>
+    <test></test>
+  </div>
+</template>
+
+<script>
+import test from "./components/test.vue";
+
+export default {
+  components: {
+    test,
+  },
+};
+</script>
+
+<style>
+html,
+body,
+template {
+  margin: 0;
+  color: white;
+}
+.container {
+  width: 100%;
+  min-height: 100vh;
+  background-color: #121212;
+}
+</style>
